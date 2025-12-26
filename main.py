ec02a02375624a68017d9291763a9182c3e9c090
zhang <1274556865@qq.com>
2025-12-26 17:25:25 +0800

    update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from typing import Tuple, Optional
 
 # 浏览器自动化相关
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
